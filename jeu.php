491feef41dbac2469dc1879553d404a3310ae81b
samuel-robinet <samuel.robinet@etu.umontpellier.fr>
2020-10-28 14:55:35 +0100

    page jeu

--- jeu.php
+++ jeu.php
@@ -2,8 +2,6 @@
 	require_once 'Model/Model.php';
 ?>
 
-
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -22,7 +20,7 @@
 		<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-fixed-top">
 			 <div class="container">
         		<div class="navbar">
-					 <a class="navbar-brand" href="#"><img src="./images/accounting.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">Le Casse de la Compta</a>
+					 <a class="navbar-brand" href="#"><img src="./images/accounting.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy"> Le Casse de la Compta</a>
 					 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					   <span class="navbar-toggler-icon"></span>
 					 </button>
@@ -30,10 +28,10 @@
 					 <div class="collapse navbar-collapse" id="navbarSupportedContent">
 					   <ul class="navbar-nav mr-auto">
 					     <li class="nav-item active">
-					       <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+					       <a class="nav-link" href="./index.php">Accueil <span class="sr-only">(current)</span></a>
 					     </li>
 					     <li class="nav-item active">
-					       <a class="nav-link" href="./jeu.php">Jeu <span class="sr-only"></span></a>
+					       <a class="nav-link" href="#">Jeu <span class="sr-only"></span></a>
 					     </li>
 					   </ul>
 					 </div>
@@ -42,37 +40,33 @@
 		</nav>
 	</header>
 
-	<div class="fond" >
-
-		<div class="pageune" >
+	
 				<br>
 				<br>
 				<br>
-			<div id="font">
+			
 				<br>
 				<br>
 				<br>
-				 <div class="container">
+			<div class="container">
 				      <div class="jumbotron">
-				        <h1>Le projet</h1>
+				        <h1>Le Casse au BDE</h1>
 				        <p></p>
-				         <p class="text-justify center">Ce projet a pour objectif de réaliser un escape game virtuel sur le thème du management et plus particulièrement de la comptabilité.</p>
-				      </div>
-
-
-					<center>
-					<?php 
-					echo "<br>";
-					echo "Page d'accueil projet S3, bonjour dieu";
-
-					 ?>
-					</center>
+				         <p class="text-justify center">Trame a créer. Vous êtes membre du BDE et vous rentrer dans le bureau du BDE.</p>
+				         <button type="submit" class="btn btn-warning" href="" role="button">Jouer</button>
+				         <a href="./intrigue1.php" class="btn btn-warning" role="button" aria-pressed="true">Jouer</a>
+				      </div> 
 			</div>
-		</div>
-	</div>
-</div>
+			<div class="container">
+				      <div class="jumbotron">
+				        <h1>Jeu 2</h1>
+				        <p></p>
+				         <p class="text-justify center"></p>
+				         <button type="button" class="btn btn-warning">Jouer</button>
+				      </div>
+			</div>
+	
 
-Page deux
 <footer>
       
 </footer>
