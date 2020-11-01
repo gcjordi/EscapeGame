2796efbb4f4f6c0641d14f47d1b4c566d593e0ee
donnettew <william.donnette@etu.umontpellier.fr>
2020-11-01 16:01:37 +0100

    JEUX

---
+++ View/jeux.php
@@ -0,0 +1,10 @@
+<div class="jeux_container">
+        <h1>Le Casse au BDE</h1>
+        <p>Trame a créer. Vous êtes membre du BDE et vous rentrer dans le bureau du BDE.</p>
+        <a href="Controller/jeu1.php" class="box">JOUER</a>
+</div>
+<div class="jeux_container">
+        <h1>Jeu 2</h1>
+        <p>Jeu Futur</p>
+</div>
+	
