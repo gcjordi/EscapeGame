ead060fb15312f94ba62a2c0f30c22438e0e641e
samuel-robinet <samuel.robinet@etu.umontpellier.fr>
2020-11-03 20:33:13 +0100

    couloir+porte il reste a faire le css, le cadenas et de mettre l'enigme. j'ai pas trouvé mieux pour la porte

--- Controller/scene.php
+++ Controller/scene.php
@@ -1,41 +1,4 @@
 <?php
-	require_once '../Model/Model.php';
-	require_once '../Model/Utilisateur.php';
 
-	session_start();
-
-	if(!isset($_SESSION['user_connected'])){
-  	header("Location:../connexion.php"); //ne pas mettre d'espace avant les ":"
-  }
 ?>
-
-
-
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Accounting & Management</title>
-  <meta charset="utf-8">
-
-  <link rel="stylesheet" type="text/css" href="../css/styles.css">
-  <link rel="stylesheet" type="text/css" href="../css/bootstrap.css"> 
-  <link rel="shortcut icon" href="../View/IMG/accounting.png" />
-
-  
-</head>
-
-<body>
-	
-
-	<div class="couloir" >
-		<div id="font">
-			
-		
-		<button id="PorteBDE">Suiv</button>
-
-		</div>
-	</div>
-	<script src="../js/script.js"></script>
-</body>
-</html>
 
---
+++ View/JS/script.js
@@ -0,0 +1,7 @@
+let porteBDE= document.querySelector('#PorteBDE');
+
+porteBDE.addEventListener('click',()=>{
+	document.getElementById("fondjeu").style.backgroundImage = 
+	"url(https://cdn.pixabay.com/photo/2019/02/07/04/21/door-3980455_1280.jpg)";
+
+});
