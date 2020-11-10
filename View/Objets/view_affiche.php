23d0aabb45d7f8aaa93772950149ca96f2e1b782
samuel-robinet <samuel.robinet@etu.umontpellier.fr>
2020-11-10 22:14:34 +0100

    maj

--- View/Objets/view_affiche.php
+++ View/Objets/view_affiche.php
@@ -8,7 +8,7 @@
       height: 700px;
       transform: translate(-50%, -50%);
       padding: 12px 30px;
-      background-image: URL(View/IMG/Copie de Votre BDE INFO.jpg);
+      background-image: URL(View/IMG/Affiche_BDE_INFO_vide.jpg);
       background-size: cover;
       width: 60vh;">
 		<div id="defs" style="margin: 30% 0% 0% 0%;"></div>
