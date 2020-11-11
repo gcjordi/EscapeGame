7bd920b34ab19c4aadc91ff0ea36c3e245b0e48d
samuel-robinet <samuel.robinet@etu.umontpellier.fr>
2020-11-11 21:03:17 +0100

    Porte BDE (code terminé)

--- View/view_porteBDE.php
+++ View/view_porteBDE.php
@@ -1,19 +1,18 @@
-<div id="porteBDE" style="background: yellow; height: 100vh; width: 100vw">
+<div id="porteBDE" style="background: #a7a192; height: 100vh; width: 100vw">
     Vous etes devant la porte du BDE
     <div class="acces" id="couloir" style="position: fixed; background: red; height: 100px; z-index: 2">
         Aller dans le Couloir
     </div>
 
 
-    <div class="show_objet" objet="cadenas" id="porte" style="    width: 300px;
-    height: 600px;
-    background-color: green;
-    position: absolute;
-    bottom: 0;
-    left: 50%;
-    transform: translate(-50%, 0px);
-    cursor: pointer; 
-    font-size: 30px;
-    color: white">PORTE !!</div>
+    <div class="show_objet" objet="cadenas" id="porte" style="
+        position: absolute;
+        transform: translate(-50%, -50%);
+        top: 50%;
+        left: 50%;
+        width: 300px;
+        height: 500px;
+        background-image: Url(View/IMG/PorteBDE.png);">
+    </div>
 
 </div>
