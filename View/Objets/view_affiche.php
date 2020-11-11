0a92bec67137a7cb2370d97ab868e18d641ead72
Adrien Segura <adrien.segura@etu.umontpellier.fr>
2020-11-11 12:51:15 +0100

    Reparation affiche

--- View/Objets/view_affiche.php
+++ View/Objets/view_affiche.php
@@ -1,11 +1,11 @@
 
 <div id="affiche" style="display: none;">
-  <div id="aff_definitions" style="display: inline-flex;
+  <div id="aff_definitions" style="
       position: absolute;
       top: 50%;
       left: 50%;
       z-index: 10;
-      height: 700px;
+      height: 90%;
       transform: translate(-50%, -50%);
       padding: 12px 30px;
       background-image: URL(View/IMG/Affiche_BDE_INFO_vide.jpg);
@@ -13,7 +13,8 @@
       width: 60vh;
       color:white;">
 		<div id="defs" style="margin: 80% 11% 0% -3%;"></div>
-		<div id="reps" style="margin: 80% -3% 0% 0%;"></div>
+		<div id="reps" style="display: flex;
+    width: 100%; height: 80px; justify-content:space-between;"></div>
 </div>
 </div>
 
