caf3bb452a47509eae4d782cd84735ad538dbfcc
samuel-robinet <samuel.robinet@etu.umontpellier.fr>
2020-11-11 21:22:00 +0100

    css textarea (figé)

--- jeu1.php
+++ jeu1.php
@@ -39,7 +39,7 @@ if(!isset($_SESSION['user_connected'])){
     <div id="container_blocnote" style="position: fixed; bottom: 5vh; right: 5vw; width: 6vh; height: 6vh; border-radius: 6vh; background: white; display: flex; flex-direction: column; justify-content: space-between; align-items: center; z-index: 10000; padding: 3vh; transition: width 300ms, height 300ms">
         <div id="blocnote" style="display: none; width: 20vw; height: 20vh">
             <p id="titre_BN" >Bloc-notes</p>
-            <textarea style="height: 19vh; width: 20vw; color: black; resize: none;"></textarea>
+            <textarea style="height: 19vh; width: 20vw;"></textarea>
         </div>
         <div class="show_bloc" style="width: 6vh; height: 6vh; display: flex; flex-direction: row; justify-content: flex-end; align-items: center; cursor: pointer; transition: width 300ms, height 300ms">
             <img src="View/IMG/bloc-note.png" alt="bloc-note" style="width: 6vh; height: 6vh">
