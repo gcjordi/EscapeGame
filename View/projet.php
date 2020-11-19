c36fc3d12546af0594ad01b8736adaa8bef8ee60
samuel-robinet <samuel.robinet@etu.umontpellier.fr>
2020-11-19 12:45:53 +0100

    mise en page accueil

--- View/projet.php
+++ View/projet.php
@@ -1,9 +1,9 @@
-<div class="accueil_container degrade_halloween box">
-    <h1>LE PROJET</h1>
+<div class="container box">
+    <h1 class="titre">LE PROJET</h1>
     <p>Ce projet a pour objectif de réaliser un escape game virtuel sur le thème du management et plus particulièrement de la comptabilité.</p>
 </div>
 
-<div class="jeux_container box">
+<div class="container box">
         <h6 class="titre">LE CASSE AU BDE,</h6>
         <p class="textegris">Plongez dans l'univers de la comptabilté au BDE ! Bienvenue au département informatique ! En ce début d’année le chef du département a des inquiétudes sur la santé financière du BDE informatique. Vous êtes amenés à vérifier si ces inquiétudes sont fondées.
 </p>
