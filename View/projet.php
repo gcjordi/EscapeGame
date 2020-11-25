c1eda597f47945426578f190e941733d0bec7d6e
samuel-robinet <samuel.robinet@etu.umontpellier.fr>
2020-11-25 14:09:48 +0100

    mise en page index

--- View/projet.php
+++ View/projet.php
@@ -1,10 +1,11 @@
 <div class="container box">
     <h5 class="titre">LE PROJET</h1>
     <p class="textegris">Ce projet a pour objectif de réaliser un escape game virtuel sur le thème du management et plus particulièrement de la comptabilité.</p>
+    <img src="https://upload.wikimedia.org/wikipedia/fr/f/f4/LogoIutMontpellier.png" alt="exemple de texte alternatif" style="width: 20%;">
 </div>
 
 <div class="container box">
-        <h6 class="titre">LE CASSE AU BDE,</h6>
+        <h6 class="titre">LE CASSE AU BDE</h6>
         <p class="textegris">Plongez dans l'univers de la comptabilté au BDE ! Bienvenue au département informatique ! En ce début d’année le chef du département a des inquiétudes sur la santé financière du BDE informatique. Vous êtes amenés à vérifier si ces inquiétudes sont fondées.
 </p>
         <div class="boxinscription">
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     $CSS = [
         'View/CSS/header2.css',
         'View/CSS/styles5.css',
+        'View/CSS/styles6.css',
         'View/CSS/projet1.css',
     ];
 
