c3514409eee45d0b9fbe52a045113faeb54ee8bf
samuel-robinet <samuel.robinet@etu.umontpellier.fr>
2020-11-25 21:02:08 +0100

    mise en page inscription.php

--- View/header.php
+++ View/header.php
@@ -18,19 +18,19 @@
     </div>
 
     <div class="right col-7">
-        <a class="item box degrade_halloween" href="classement.php">
+        <a class="item degrade_halloween" href="classement.php">
             <p class="item_title">Classement</p>
         </a>
         <?php if(!isset($_SESSION["user_connected"])): ?>
-        <a class="item box degrade_halloween" href="inscription.php">
+        <a class="item degrade_halloween" href="inscription.php">
             <p class="item_title">Inscription</p>
         </a>
-        <a class="item box degrade_halloween" href="connexion.php">
+        <a class="item degrade_halloween" href="connexion.php">
             <p class="item_title">Connexion</p>
         </a>
         <?php endif;
         if (isset($_SESSION["user_connected"])): ?>
-        <a class="item box degrade_halloween" href="profil.php">
+        <a class="item degrade_halloween" href="profil.php">
             <p class="item_title">Mon Compte</p>
         </a>
         <?php endif; ?>
--- View/inscription.php
+++ View/inscription.php
@@ -1,20 +1,35 @@
+
+
 <div class="container_inscription">
-
-
-
-<form method="post" class="degrade_halloween box">
+    <form method="post">
     <h1>Inscris-toi !</h1>
     <?= $erreur != "" ? '<p style="color: red;">'.htmlspecialchars($erreur).'</p>' : '' ?>
-    <label for="identifiant">Pseudo :</label>
-    <input type="text" id="identifiant" name="identifiant" value="<?= htmlspecialchars($identifiant) ?>" required>
-    <label for="mdp">Mot de Passe :</label>
-    <small>6 caractères minimums </small>
-    <input type="password" id="mdp" name="mdp" required>
-    <label for="remdp">Mot de Passe a nouveau :</label>
-    <input type="password" id="remdp" name="remdp" required>
-    <input type="submit" name="inscrire" value="S'inscrire">
-    <p>Ou si tu as déjà un compte !</p>
-    <a href="./connexion.php" class="">Connecte toi !</a>
-    
-</form>
+        <div class="groupe"> 
+            <img src="View/IMG/icon/id-card.svg" alt="Nom d'utilisateur" class="inputImg">
+            <input type="text" id="identifiant" name="identifiant" value="<?= htmlspecialchars($identifiant) ?>" placeholder="Nom d'utilisateur" required>
+        
+
+        </div>
+        <div class="groupe">
+            <img src="View/IMG/icon/padlock.svg" alt="Mot de passe" class="inputImg">
+            <input type="password" id="mdp" name="mdp" placeholder="Mot de passe de six caractères" required>
+        
+        </div>
+        <div class="groupe">
+            <img src="View/IMG/icon/padlock.svg" alt="Mot de passe" class="inputImg">
+            <input type="password" id="remdp" name="remdp"  placeholder="Verification mot de passe" required>
+        </div>
+        <div class="groupeCGU">
+            <input type="checkbox" class="checkbox" id="checkbox" required />
+            <label for="checkbox"></label>
+            <p class="cgu">En m'inscrivant, j'accepte les <a href="/cgv" class="rouge">CGV</a> et les <a href="/cgu" class="rouge">CGU</a>, notamment l'utilisation de cookies.</p>
+        </div>
+
+         <input type="submit" name="inscrire" class="boutton" value="S'inscrire">
+    </form>
+
+    <div class="groupePasCompte">
+    <p>Vous avez déjà un compte ? <a href="./connexion.php" class="rouge">Connexion</a></p>
+    </div> 
 </div>
+
