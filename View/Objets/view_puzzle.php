c0a4617d0e44a3471219b5c19d0279dc5ae2bd68
donnettew <william.donnette@etu.umontpellier.fr>
2020-12-01 11:23:54 +0100

    PUZZLE

--- View/Objets/view_puzzle.php
+++ View/Objets/view_puzzle.php
@@ -1,16 +1,12 @@
 <style type="text/css">
-    #dropZone,
-    #animals {
+    #dropZone, #animals {
         display: flex;
         flex-flow: row nowrap;
         justify-content: space-between;
         min-height: 200px;
         margin-bottom: 20px;
     }
-    #dropBox1,
-    #dropBox2,
-    #dropBox3,
-    #dropBox4 {
+    #dropBox1, #dropBox2, #dropBox3, #dropBox4 {
         display: flex;
         flex-flow: column nowrap;
         justify-content: center;
@@ -36,68 +32,24 @@
     width: 50vw;
     background-color: purple">
     <div id="dropZone">
-        <div
-                id="dropBox1"
-                ondrop="drop(event)"
-                ondragover="allowDrop(event)"
-        >
+        <div id="dropBox1" ondrop="drop(event)" ondragover="allowDrop(event)">
             <p>Fish</p>
         </div>
-        <div
-                id="dropBox2"
-                ondrop="drop(event)"
-                ondragover="allowDrop(event)"
-        >
+        <div id="dropBox2" ondrop="drop(event)" ondragover="allowDrop(event)">
             <p>Mouse</p>
         </div>
-        <div
-                id="dropBox3"
-                ondrop="drop(event)"
-                ondragover="allowDrop(event)"
-        >
+        <div id="dropBox3" ondrop="drop(event)" ondragover="allowDrop(event)">
             <p>Cat</p>
         </div>
-        <div
-                id="dropBox4"
-                ondrop="drop(event)"
-                ondragover="allowDrop(event)"
-        >
+        <div id="dropBox4" ondrop="drop(event)" ondragover="allowDrop(event)">
             <p>Dog</p>
         </div>
     </div>
-    <div
-            id="animals"
-            ondrop="drop(event)"
-            ondragover="allowDrop(event)"
-    >
-        <img
-                id="cat"
-                src="https://www.veterinarypracticenews.com/wp-content/uploads/2019/10/bigstock-Four-Kittens-On-A-White-Backgr-2730282.jpg"
-                draggable="true"
-                ondragstart="drag(event)"
-                width="300"
-        />
-        <img
-                id="dog"
-                src="https://www.wallpaperflare.com/static/800/62/574/puppies-white-background-paws-yellow-wallpaper-preview.jpg"
-                draggable="true"
-                ondragstart="drag(event)"
-                width="300"
-        />
-        <img
-                id="mouse"
-                src="https://www.adamspestcontrol.com/cms-files/size-992x992/adams-mouse-600x600.jpg"
-                draggable="true"
-                ondragstart="drag(event)"
-                width="300"
-        />
-        <img
-                id="fish"
-                src="https://cdn.mos.cms.futurecdn.net/uhLVL2jTdtQ7ScXCeoeAU6-650-80.jpg.webp"
-                draggable="true"
-                ondragstart="drag(event)"
-                width="300"
-        />
+    <div id="animals" ondrop="drop(event)" ondragover="allowDrop(event)">
+        <img id="cat" src="https://www.veterinarypracticenews.com/wp-content/uploads/2019/10/bigstock-Four-Kittens-On-A-White-Backgr-2730282.jpg" draggable="true" ondragstart="drag(event)" width="300"/>
+        <img id="dog" src="https://www.wallpaperflare.com/static/800/62/574/puppies-white-background-paws-yellow-wallpaper-preview.jpg" draggable="true" ondragstart="drag(event)" width="300"/>
+        <img id="mouse" src="https://www.adamspestcontrol.com/cms-files/size-992x992/adams-mouse-600x600.jpg" draggable="true" ondragstart="drag(event)" width="300"/>
+        <img id="fish" src="https://cdn.mos.cms.futurecdn.net/uhLVL2jTdtQ7ScXCeoeAU6-650-80.jpg.webp" draggable="true" ondragstart="drag(event)" width="300"/>
     </div>
 </div>
 <script type="text/javascript">
