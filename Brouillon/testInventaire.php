caf31e3b12d08bc7af2cf2df73d1702d4061088d
Adrien Segura <adrien.segura@etu.umontpellier.fr>
2020-12-18 17:20:15 +0100

    Inventaire

--- Brouillon/testInventaire.php
+++ Brouillon/testInventaire.php
@@ -4,14 +4,16 @@
 
 <img width="30" src="../View/IMG/inventaire/clef.png" id="key2" objet='<?= json_encode(['id' => 'key2', 'nom' => 'clé', 'img' => "../View/IMG/inventaire/clef.png"]) ?>' ondragstart="drag(event)">
 <img width="30" src="../View/IMG/affiche_invitation.png" id="affiche1" objet='<?= json_encode(['id' => 'affiche1', 'nom' => 'affiche', 'img' => "../View/IMG/affiche_invitation.png"]) ?>' ondragstart="drag(event)">
-<div id="delete" objet="key1">Delete</div>
-<div id="deplace" objet="key1" newSlot="0">Deplace</div>
+
 <div id="affichage" style="display: inline-flex;"></div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 
 <script type="text/javascript">
-
+	var div = document.getElementById('affichage')
+        
+        inventaire = new Inventaire(5, div);
+        inventaire.affiche()
 
 	function allowDrop(event) {
         event.preventDefault();
@@ -20,15 +22,7 @@
         event.dataTransfer.setData("text", event.target.id);
     }
 
-	$('#delete').on('click', function(e){
-		var objet = $(this).attr('objet')
-		var objet = inventaire.getItem(objet);
-		
-		if(objet != null){
-			inventaire.deleteItem(objet)
-			inventaire.affiche(affichage);
-		}
-	})
+	
 
 	affichage.ondrop = function(event){
 		
@@ -62,29 +56,11 @@
 		}
 
 
-		inventaire.affiche(affichage);
+		inventaire.affiche();
 
 	}
 
 
-	$('#ajouter').on('click', function(e){
-		var json = JSON.parse($(this).attr('objet'))
-		var objet = new Objet(json.id, json.nom, json.img)
-		inventaire.saveItem(objet)
-		inventaire.affiche(affichage);
-
-	})
-
-	$('#deplace').on('click', function(e){
-		var objet = $(this).attr('objet')
-		var slot = $(this).attr('newSlot')
-		var objet = inventaire.getItem(objet);
-		if(objet != null){
-			inventaire.deplaceItem(slot, objet)
-			inventaire.affiche(affichage);
-		}
-		
-
-	})
+	
 
 </script>
