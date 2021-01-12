0c0d28145dfbe621a61a4ea3b28124fd2fcc4c11
samuel-robinet <samuel.robinet@etu.umontpellier.fr>
2021-01-12 10:39:30 +0100

    corr

--- View/projet.php
+++ View/projet.php
@@ -1,7 +1,7 @@
 <div class="container">
         <h6 class="titre">LE CASSE AU BDE</h6>
         <p class="textegris">Bienvenue au département Informatique de l'IUT Montpellier-Sète ! En ce début d’année le chef du département a des inquiétudes sur la santé financière du Bureau des Etudiants (BDE) Informatique. Vous êtes amenés à vérifier si ces inquiétudes sont fondées ou non.
-N'attendez pas et plongez dans l'univers de la comptabilté du BDE pour vérifier les inquiétudes du chef du département... 
+N'attendez pas et plongez dans l'univers de la comptabilté du BDE pour vérifier les inquiétudes du chef du département. 
 </p>
         <div class="boxbas">
                           <div class="bar"></div>
@@ -14,12 +14,12 @@ N'attendez pas et plongez dans l'univers de la comptabilté du BDE pour vérifie
 <div class="container">
     <h5 class="titre">LE PROJET</h1>
     <p class="textegris">Ce projet a pour objectif de réaliser un escape game virtuel sur le thème du management et plus particulièrement de la comptabilité.</p>
-    <p> Réalisé par Samuel ROBINET - Adrien SEGURA - William DONNETTE - Santiago MARTINEZ </p>
-    <p> Sous la direction de Antoine CHOLLET </p>
+    <p class="textegris"> Réalisé par Samuel ROBINET - Adrien SEGURA - William DONNETTE - Santiago MARTINEZ </p>
+    <p class="textegris"> Sous la direction de Antoine CHOLLET </p>
     <div class="boxbas">
         <div class="barlogo"></div>
         <div class="masque"></div>
-        <img src="https://upload.wikimedia.org/wikipedia/fr/f/f4/LogoIutMontpellier.png" alt="exemple de texte alternatif" style="width: 19%; position: relative;">
+        <img src="https://upload.wikimedia.org/wikipedia/fr/f/f4/LogoIutMontpellier.png" alt="logo" style="width: 19%; position: relative;">
     </div>
     
 </div>
