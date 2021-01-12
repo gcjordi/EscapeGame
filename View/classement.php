e55ae9cce0098b31c892b34272fd4c9e6336279a
Adrien Segura <adrien.segura@etu.umontpellier.fr>
2021-01-13 00:20:23 +0100

    Correction classement

--- View/classement.php
+++ View/classement.php
@@ -6,6 +6,7 @@ $classement = Classement::getClassement();
 $pos = 1;
 
 
+
 function convertSecondToMinute($second){
     if($second > 59){
         $minute = intval($second/60);
@@ -56,9 +57,9 @@ function convertSecondToMinute($second){
     </tr>
 <?php else : ?>
     <?php foreach($classement as $user): ?>
-        <tr class="ligne <?= $user->getAttr('id') == $_SESSION['user_connected']->getAttr('id') ? 'you' : ''?>">
+        <tr class="ligne <?= isset($_SESSION['user_connected']) && $user->getAttr('id') == $_SESSION['user_connected']->getAttr('id') ? 'you' : ''?>">
             <td><?= $pos ?></td>
-            <td><?= $user->getAttr('id') == $_SESSION['user_connected']->getAttr('id') ? htmlspecialchars($user->getAttr("identifiant"))." (vous)" :htmlspecialchars($user->getAttr("identifiant")) ?></td>
+            <td><?= isset($_SESSION['user_connected']) && $user->getAttr('id') == $_SESSION['user_connected']->getAttr('id') ? htmlspecialchars($user->getAttr("identifiant"))." (vous)" :htmlspecialchars($user->getAttr("identifiant")) ?></td>
             <td><?= convertSecondToMinute(htmlspecialchars($user->getAttr("temps"))) ?></td>
             <td><?= htmlspecialchars($user->getAttr("tentative")) ?></td>
         </tr>
