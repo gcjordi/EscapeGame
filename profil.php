7a9d00ccf5cd0b621adc24dbfd957a00afec77fc
Adrien Segura <adrien.segura@etu.umontpellier.fr>
2021-01-20 16:57:18 +0100

    Réparation cadenas

--- profil.php
+++ profil.php
@@ -7,7 +7,7 @@ $CSS = [
   'View/CSS/header2.css',
 	'View/CSS/styles9.css',
   'View/CSS/styles6.css',
-  'View/CSS/inscription1.css',
+  'View/CSS/inscription2.css',
   'View/CSS/classement7.css'
 
 ];
